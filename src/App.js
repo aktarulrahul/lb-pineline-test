8c284e7dfe237ef5d2757804ade6c666786804c6
aktarulrahul <contact.aktarul@gmail.com>
2022-02-03 14:20:54 +0600

    main init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello React & Tailwind!
+    <h1 className="text-3xl font-bold border rounded bg-green-300 m-4 p-4 text-center">
+      Hello, I'm the Main Branch for Production.
     </h1>
   );
 }
