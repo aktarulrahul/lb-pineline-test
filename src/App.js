83fe02f99632a0c54e25b1105cf5809a12650727
aktarulrahul <contact.aktarul@gmail.com>
2022-02-03 14:26:56 +0600

    feature-a added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import FeatureA from "./components/FeatureA/FeatureA";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold border rounded bg-green-300 m-4 p-4 text-center">
-      Hello, I'm the Main Branch for Production.
+    <div className="border rounded bg-green-300 m-4 p-4 ">
+    <h1 className="text-3xl font-bold text-center">
+    Hello, I'm the Main Branch for Production.
     </h1>
+    <FeatureA />
+    </div>
   );
 }
 
---
+++ src/components/FeatureA/FeatureA.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeatureA = () => {
+  return (
+    <div className='text-xl text-center bg-blue-600 text-white border rounded m-3 p-4'>
+      Feature A is here.
+    </div>
+  );
+};
+
+export default FeatureA;
